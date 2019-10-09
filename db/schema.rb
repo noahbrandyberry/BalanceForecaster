759309bb142f35b23e53c62aa1b1e9f72992b57e
noahbrandyberry <noahbrandyberry@gmail.com>
2019-10-08 23:19:03 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,67 +10,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_164056) do
+ActiveRecord::Schema.define(version: 2019_08_06_015821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "accounts", force: :cascade do |t|
+  create_table "access_tokens", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "accounts", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.string "name", default: "", null: false
     t.decimal "balance", precision: 8, scale: 2, default: "0.0", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "forecast_items", force: :cascade do |t|
-    t.integer "item_id"
-    t.date "date", null: false
-    t.date "new_date"
-    t.boolean "deleted", default: false, null: false
+  create_table "categories", id: :serial, force: :cascade do |t|
     t.string "name"
-    t.boolean "is_bill"
-    t.decimal "amount", precision: 8, scale: 2
-    t.boolean "continues", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "items", force: :cascade do |t|
+  create_table "forecast_items", id: :serial, force: :cascade do |t|
+    t.integer "item_id"
+    t.date "date"
+    t.decimal "amount", precision: 8, scale: 2
+    t.boolean "continues", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "new_date"
+    t.boolean "deleted"
+    t.string "name", default: ""
+    t.boolean "is_bill", default: false
+  end
+
+  create_table "items", id: :serial, force: :cascade do |t|
     t.integer "account_id"
+    t.integer "user_id"
     t.string "name", default: "", null: false
     t.boolean "is_bill", default: false, null: false
     t.boolean "repeat", default: false, null: false
-    t.integer "repeat_frequency", default: 1
-    t.integer "repeat_type", default: 2
-    t.decimal "amount", precision: 8, scale: 2, default: "0.0"
+    t.integer "repeat_frequency"
+    t.string "repeat_type"
+    t.decimal "amount", precision: 8, scale: 2, default: "0.0", null: false
     t.date "start_date", null: false
     t.date "end_date"
-    t.text "note"
+    t.string "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_transfer", default: false, null: false
+    t.integer "transfer_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table "selected_items", id: :serial, force: :cascade do |t|
+    t.integer "account_id"
+    t.integer "item_id"
+    t.string "name"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "versions", id: :serial, force: :cascade do |t|
+    t.string "number"
+    t.string "description"
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
